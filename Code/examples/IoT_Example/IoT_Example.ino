0361053a83db9e33a4243bbc77a300d75405fb86
Timothy Woo <botletics@gmail.com>
2017-11-22 12:12:32 -0500

    Update IoT_Example.ino

--- Code/examples/IoT_Example/IoT_Example.ino
+++ Code/examples/IoT_Example/IoT_Example.ino
@@ -1,1 +1,254 @@
+/* This is an example sketch to send data to dweet.io, a free cloud API. Thi code
+ *  also puts the AVR microcontroller to sleep to save power, as well as the MCP9808
+ *  temperature sensor.
+ *  
+ *  Written by: Timothy Woo (www.botletics.com)
+ *  Github repo: https://github.com/botletics/NB-IoT-Shield
+ *  Last Updated: 11/22/2017
+  */
 
+#include "Adafruit_FONA.h"
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+// Libraries for temperature sensor
+#include <Wire.h>
+#include "Adafruit_MCP9808.h"
+
+// Create the MCP9808 temperature sensor object
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+// Default
+//#define FONA_RX 2
+//#define FONA_TX 3
+//#define FONA_RST 4
+//#define PWRKEY 5
+
+// For Feather FONA (SIM800) specifically
+//#define FONA_RX  9
+//#define FONA_TX  8
+//#define FONA_RST 4
+//#define FONA_RI  7
+// For the PWRKEY pin, use any available digital pin, but cut the
+// PWRKEY trace on the Feather FONA for this to work. YOu can't sleep
+// the SIM800 if the trace isn't cut.
+//#define PWRKEY 0
+
+// For LTE shield
+#define FONA_PWRKEY 3
+//#define FONA_DTR 4 // Can be used to wake up SIM7000 from sleep
+#define FONA_RI 5 // Need to enable via AT commands
+#define FONA_RX 7
+#define FONA_TX 6
+#define FONA_RST 8
+
+// Using SoftwareSerial:
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+// Hardware serial is also possible!
+//  HardwareSerial *fonaSerial = &Serial1;
+
+// Use this for FONA 800 and 808s
+//Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+// Use this one for FONA 3G. Run the "FONA3G_setBaud" example before running code for 3G or it may not work.
+//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
+// Use this one for FONA LTE
+// Notice how we don't include the reset pin because it's reserved for emergencies on the LTE module!
+Adafruit_FONA_LTE fona = Adafruit_FONA_LTE();
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+char imei[16] = {0}; // Use this for device ID
+char replybuffer[255]; // Large buffer for replies
+uint8_t type;
+uint16_t VBAT = 0; // Battery voltage
+uint16_t battLevel = 0; // Battery level (percentage)
+
+void setup() {
+  Serial.begin(115200);
+
+  tempsensor.wake(); // Wake up the MCP9808 if it was sleeping
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find the MCP9808!");
+    while (1);
+  }
+  
+  pinMode(FONA_PWRKEY, OUTPUT);
+  digitalWrite(FONA_PWRKEY, LOW);
+  delay(1050); // Pull low at least 1s to turn on SIM800
+  digitalWrite(FONA_PWRKEY, HIGH);
+
+  fonaSerial->begin(115200); // Default LTE shield baud rate
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+  }
+
+  // Baud rate setup
+  fona.setBaudrate(4800); // Set to 4800 baud
+  fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while (1); // This makes the code freeze when it can't find the device
+  }
+  
+  type = fona.type();
+  Serial.println(F("FONA is OK"));
+  Serial.print(F("Found "));
+  switch (type) {
+    case FONA800L:
+      Serial.println(F("FONA 800L")); break;
+    case FONA800H:
+      Serial.println(F("FONA 800H")); break;
+    case FONA808_V1:
+      Serial.println(F("FONA 808 (v1)")); break;
+    case FONA808_V2:
+      Serial.println(F("FONA 808 (v2)")); break;
+    case FONA3G_A:
+      Serial.println(F("FONA 3G (American)")); break;
+    case FONA3G_E:
+      Serial.println(F("FONA 3G (European)")); break;
+    default: 
+      Serial.println(F("???")); break;
+  }
+  
+  // Print module IMEI number.
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+
+  // Optionally configure a GPRS APN, username, and password.
+  // You might need to do this to access your network's GPRS/data
+  // network.  Contact your provider for the exact APN, username,
+  // and password values.  Username and password are optional and
+  // can be removed, but APN is required.
+  //fona.setGPRSNetworkSettings(F("your APN"), F("your username"), F("your password"));
+  //fona.setGPRSNetworkSettings(F("phone"); // This worked fine for a standard AT&T 3G SIM card (US)
+  fona.setGPRSNetworkSettings(F("m2m.com.attz")); // For AT&T IoT SIM card
+
+  // Optionally configure HTTP gets to follow redirects over SSL.
+  // Default is not to follow SSL redirects, however if you uncomment
+  // the following line then redirects over SSL will be followed.
+  //fona.setHTTPSRedirect(true);
+}
+
+void loop() {
+  // Connect to cell network and verify connection
+  // If unsuccessful, keep retrying every 2s until a connection is made
+  while (!netStatus()) {
+    Serial.println(F("Failed to connect to cell network, retrying..."));
+    delay(2000); // Retry every 2s
+  }
+  Serial.println(F("Connected to cell network!"));
+  delay(1000); // At least 1s to help GPRS enable successfully
+
+  // Measure battery level
+//  VBAT = readVcc(); // Voltage
+  battLevel = readVcc(); // Percentage
+
+  // Measure temperature
+  tempsensor.wake(); // Wake up the MCP9808 if it was sleeping
+  float tempC = tempsensor.readTempC();
+  float tempF = tempC * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(tempC); Serial.print("*C\t"); 
+  Serial.print(tempF); Serial.println("*F");
+  
+  Serial.println("Shutting down the MCP9808...");
+  tempsensor.shutdown(); // In this mode the MCP9808 draws only about 0.1uA
+
+  float temperature = tempC; // Select what unit you want to use for this example
+  
+  // Turn on GPRS
+  while (!fona.enableGPRS(true)) {
+    Serial.println(F("Failed to enable GPRS, retrying..."));
+    delay(2000); // Retry every 2s
+  }
+  Serial.println(F("Enabled GPRS!"));
+
+  // Post something like temperature and battery level to the web API
+  // Construct URL and post the data to the web API
+  char URL[150]; // Make sure this is long enough for your request URL
+  char tempBuff[16];
+  char battLevelBuff[16];
+
+  // Format the floating point numbers as needed
+  dtostrf(temperature, 1, 0, tempBuff); // float_val, min_width, digits_after_decimal, char_buffer
+  dtostrf(battLevel, 1, 0, battLevelBuff);
+
+  // Construct the appropriate URL's and body, depending on request type
+  // Use IMEI as device ID for this example
+
+  // GET request
+  sprintf(URL, "http://dweet.io/dweet/for/%s?temp=%s&batt=%s", imei, tempBuff, battLevelBuff);
+
+  if (!fona.postData("GET", URL, "")) { // Add the quotes "" as third input because for GET request there's no "body"
+    Serial.println(F("Failed to post data, retrying..."));
+  }
+  
+  // You can also do a POST request instead
+  /*
+  sprintf(URL, "http://dweet.io/dweet/for/%s", imei);
+  sprintf(body, "{\"temp\":%s,\"batt\":%s}", tempBuff, battLevelBuff);
+  
+  if (!fona.postData("POST", URL, body)) {
+    Serial.println(F("Failed to complete HTTP POST..."));
+  }
+  */
+
+  // Disable GPRS
+  // Note that you might not want to check if this was successful, but just run it
+  // since the next command is to turn off the module anyway
+  if (!fona.enableGPRS(false)) Serial.println(F("Failed to disable GPRS"));
+
+  // Power off FONA
+  // You should see the "PWR" LED turn off after this command
+  if (!fona.powerDown()) Serial.println(F("Failed to power down FONA!"));
+  
+  // Alternative to the AT command method above:
+  // If your FONA has a PWR_key pin connected to your MCU, you can pulse the PWR_key
+  // LOW for a little bit, then pull it back HIGH, like this:
+//  digitalWrite(PWRKEY, LOW);
+//  delay(600); // Minimum of 64ms to turn on and 500ms to turn off for FONA3G. Check spec sheet for other types
+//  digitalWrite(PWRKEY, HIGH);
+  
+  // Shut down the MCU
+  MCU_powerDown();
+}
+
+// Read the battery level percentage
+float readVcc() {
+//  if (!fona.getBattVoltage(&VBAT)) Serial.println(F("Failed to read Batt"));
+//  else Serial.print(F("VBAT = ")); Serial.print(VBAT); Serial.println(F(" mV"));
+
+  // Read battery percentage
+  if (!fona.getBattPercent(&battLevel)) Serial.println(F("Failed to read batt"));
+  else Serial.print(F("BAT % = ")); Serial.print(battLevel); Serial.println(F("%"));
+
+  return battLevel;
+}
+
+bool netStatus() {
+  int n = fona.getNetworkStatus();
+  
+  Serial.print(F("Network status ")); Serial.print(n); Serial.print(F(": "));
+  if (n == 0) Serial.println(F("Not registered"));
+  if (n == 1) Serial.println(F("Registered (home)"));
+  if (n == 2) Serial.println(F("Not registered (searching)"));
+  if (n == 3) Serial.println(F("Denied"));
+  if (n == 4) Serial.println(F("Unknown"));
+  if (n == 5) Serial.println(F("Registered roaming"));
+
+  if (!(n == 1 || n == 5)) return false;
+  else return true;
+}
+
+// Turn off the MCU completely. Can only wake up from RESET button
+// However, this can be altered to wake up via a pin change interrupt
+void MCU_powerDown() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  ADCSRA = 0; // Turn off ADC
+  power_all_disable ();  // Power off ADC, Timer 0 and 1, serial interface
+  sleep_enable();
+  sleep_cpu();
+}
