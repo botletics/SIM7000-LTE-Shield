cfb4be87f38052b84b7e4e4612ab4b9563e788e4
Timothy Woo <botletics@gmail.com>
2017-12-09 14:27:06 -0500

    Update FONA_LTE_setbaud.ino

--- Code/examples/FONA_LTE_setbaud/FONA_LTE_setbaud.ino
+++ Code/examples/FONA_LTE_setbaud/FONA_LTE_setbaud.ino
@@ -30,10 +30,13 @@
 //#define FONA_RST 4
 
 // For LTE shield
-#define FONA_PWRKEY 3
-#define FONA_RX 7
-#define FONA_TX 6
-#define FONA_RST 8
+#define FONA_PWRKEY 6
+#define FONA_RST 7
+//#define FONA_DTR 8 // Connect with solder jumper
+//#define FONA_RI 9 // Need to enable via AT commands
+#define FONA_TX 10 // Microcontroller RX
+#define FONA_RX 11 // Microcontroller TX
+//#define T_ALERT 12 // Connect with solder jumper
 
 // this is a large buffer for replies
 char replybuffer[255];
