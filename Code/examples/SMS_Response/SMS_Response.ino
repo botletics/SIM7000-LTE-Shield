44393ef7be5feaf2be43cb650754d1445dceb36c
Timothy Woo <botletics@gmail.com>
2018-10-21 16:59:10 -0400

    Added short delay after AT+IPR

--- Code/examples/SMS_Response/SMS_Response.ino
+++ Code/examples/SMS_Response/SMS_Response.ino
@@ -5,7 +5,7 @@
  *  
  *  Author: Timothy Woo (www.botletics.com)
  *  Github: https://github.com/botletics/NB-IoT-Shield
- *  Last Updated: 10/20/2018
+ *  Last Updated: 10/21/2018
  *  License: GNU GPL v3.0
  */
 
@@ -130,6 +130,7 @@ void setup() {
   
   Serial.println(F("Configuring to 9600 baud"));
   fonaSS.println("AT+IPR=9600"); // Set baud rate
+  delay(100); // Short pause to let the command run
   fonaSS.begin(9600);
   if (! fona.begin(fonaSS)) {
     Serial.println(F("Couldn't find FONA"));
