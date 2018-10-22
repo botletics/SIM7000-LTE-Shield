f9079adbc4ced9b2c391d8e9986a23512e57e6cf
Timothy Woo <botletics@gmail.com>
2018-10-21 21:15:37 -0400

    Add files via upload

--- Code/examples/FTP_Demo/FTP_Demo.ino
+++ Code/examples/FTP_Demo/FTP_Demo.ino
@@ -1,18 +1,26 @@
-/* This is a simple example sketch to send or receive files via FTP protocol.
+/*  This is a simple example sketch to send or receive files via FTP protocol. The
+ *  library supports FTP connect, GET/PUT, extended GET/PUT for larger files, rename,
+ *  delete, and disconnect methods.
+ *  
+ *  NOTE: This code (and library FTP functions) is still in progress!
  *  
  *  Should work on SIMCom modules that support FTP commands: SIM800/900/7000
- *  
- *  NOTE: This code is still in progress!
  *  
  *  FTP Image Transfer Tutorial: https://github.com/botletics/SIM7000-LTE-Shield/wiki/FTP-Image-Transfer-Tutorial
  *  
  *  Author: Timothy Woo (www.botletics.com)
  *  Github: https://github.com/botletics/SIM7000-LTE-Shield
- *  Last Updated: 10/17/2018
+ *  Last Updated: 10/21/2018
  *  License: GNU GPL v3.0
  */
 
 #include "Adafruit_FONA.h" // https://github.com/botletics/SIM7000-LTE-Shield/tree/master/Code
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+const int CS_pin = 53;
 
 // Define *one* of the following lines:
 //#define SIMCOM_2G // SIM800/808/900/908, etc.
@@ -81,7 +89,6 @@ SoftwareSerial *fonaSerial = &fonaSS;
 
 uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 uint8_t type;
-char replybuffer[255]; // this is a large buffer for replies
 char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
 
 void setup() {
@@ -134,38 +141,115 @@ void setup() {
   Serial.println(F("Enabled data!"));
 
   // Connect to FTP server
+  Serial.println(F("Connecting to FTP server..."));
   while (!fona.FTP_Connect(serverIP, serverPort, username, password)) {
     Serial.println(F("Failed to connect to FTP server!"));
     delay(2000);
   }
 
+  // Initialize SD card
+  Serial.print(F("Initializing SD card... "));
+  
+  if (!SD.begin(CS_pin)) {
+    Serial.println(F("failed!"));
+    while (1);
+  }
+  Serial.println(F("done!"));
+
   // Read the contents of a text file in the root directory of the FTP server
   // Make sure the file exists on your FTP server in the specified directory!
-  if (!fona.FTP_GET("test.txt", "/", 1024)) {
-    Serial.println(F("Failed to read file!"));
+  Serial.println(F("Reading file from FTP server..."));
+  
+//  char replyContent[255];
+//  if (!fona.FTP_GET("test.txt", "/", 1024, &* replyContent)) {
+//    Serial.println(F("Failed to read file from FTP server!"));
+//  }
+//  else {
+//    Serial.println(replyContent);
+//  }
+
+  char * readContent = fona.FTP_GET("test.txt", "/", 1024);
+  Serial.println(readContent); // DEBUG
+  free(readContent); // Free up memory alloc
+
+  // Write the content we just read from the FTP file onto our local copy
+  if (!writeToFile("test.txt", readContent)) Serial.println(F("Failed to write to file!"));
+
+  // Close FTP connection for good measure before performing next task
+  if (!fona.FTP_Quit()) {
+    Serial.println(F("Failed to close FTP connection!"));
   }
 
+  // Connect again to FTP server
+  Serial.println(F("Connecting to FTP server..."));
+  while (!fona.FTP_Connect(serverIP, serverPort, username, password)) {
+    Serial.println(F("Failed to connect to FTP server!"));
+    delay(2000);
+  }
+  
   // Upload text to a file in the server's root directory
   const char* content = "This is an FTP test 123!"; // Text to put in the file. Could be from an SD card, etc.
 
   // Upload a new file
+  Serial.println(F("Uploading file to FTP server..."));
   if (!fona.FTP_PUT("upload.txt", "/", content, strlen(content))) { // File name, file path, content, content length
     Serial.println(F("Failed to upload!"));
   }
 
-  /*
-  // Rename a file
+  // Let's rename the file we just uploaded!
+  Serial.println(F("Renaming file on FTP server..."));
   if (!fona.FTP_Rename("/", "upload.txt", "newFile.txt")) { // Path, old name, new name
     Serial.println(F("Failed to change file name!"));
   }
 
-  // Delete a file
+  // You can also delete it if you want. I am leaving this commented
+  // out so you can check that it was really there to begin with.
+  // Uncomment then upload again and watch it disappear!
+  /*
+  Serial.println(F("Deleting file from FTP server..."));
   if (!fona.FTP_Delete("newFile.txt", "/")) { // File name, file path
     Serial.println(F("Failed to delete file!"));
   }
   */
 
+  // Get the timestamp of when the file we just renamed was last modified
+//  char timestamp[20];
+  uint16_t year;
+  uint8_t month, day, hour, minute, second;
+
+  Serial.println(F("Checking last modified timestamp..."));
+  if (!fona.FTP_MDTM("newFile.txt", "/", &year, &month, &day, &hour, &minute, &second)) {
+    Serial.println(F("Failed to get timestamp!"));
+  }
+  else {
+    Serial.print(F("Year: ")); Serial.println(year);
+    Serial.print(F("Month: ")); Serial.println(month);
+    Serial.print(F("Day: ")); Serial.println(day);
+    Serial.print(F("Hour: ")); Serial.println(hour);
+    Serial.print(F("Minute: ")); Serial.println(minute);
+    Serial.print(F("Second: ")); Serial.println(second);
+  }
+
+  // Now the really cool part! We're going to upload
+  // a picture to the server using the extended PUT
+  // method (auto-detected inside the FTP_PUT method
+  // based on the content size
+  /*
+  uint32_t fileSize;
+  char * uploadContent = readFromFile("test.png", &fileSize);
+
+  Serial.print("File size: "); Serial.print(fileSize); Serial.println(F(" bytes"));
+
+  // Upload picture via FTP
+  if (!fona.FTP_PUT("test.png", "/", uploadContent, fileSize)) { // File name, file path, content, content length
+    Serial.println(F("Failed to upload!"));
+  }
+  */
+
   // Close FTP connection
+  // Note that with FTP GET/PUT requests, connection to FTP server is automatically
+  // closed after the request is successfully completed so the following function
+  // might give an error.
   if (!fona.FTP_Quit()) {
     Serial.println(F("Failed to close FTP connection!"));
   }
@@ -198,9 +282,10 @@ void moduleSetup() {
   // Press reset button if the module is still turning on and the board doesn't find it.
   // When the module is on it should communicate right after pressing reset
   fonaSS.begin(115200); // Default SIM7000 shield baud rate
-  
+
   Serial.println(F("Configuring to 9600 baud"));
   fonaSS.println("AT+IPR=9600"); // Set baud rate
+  delay(100);
   fonaSS.begin(9600);
   if (! fona.begin(fonaSS)) {
     Serial.println(F("Couldn't find FONA"));
@@ -261,6 +346,9 @@ void moduleSetup() {
   if (imeiLen > 0) {
     Serial.print("Module IMEI: "); Serial.println(imei);
   }
+
+  // Needed for rare cases in which firmware on SIM7000 sets CFUN to 0 on start-up
+  fona.setFunctionality(1); // Enable cellular (RF) with AT+CFUN=1
 }
 
 bool netStatus() {
@@ -276,4 +364,44 @@ bool netStatus() {
 
   if (!(n == 1 || n == 5)) return false;
   else return true;
+}
+
+// Write to a file in the SD card
+bool writeToFile(const char fileName, char * content) {
+  myFile = SD.open(fileName, FILE_WRITE);
+
+  // If the file opened successfully, write to it
+  if (myFile) {
+    Serial.print("Writing to file...");
+    myFile.println(content); // Write the desired content onto the file
+    myFile.close(); // Close the file
+    Serial.println(" done!");
+  }
+  else {
+    Serial.println("Error opening file!");
+    return false;
+  }
+  
+  return true;
+}
+
+// Read the contents of a file in the SD card
+char readFromFile(const char fileName, uint32_t * fileSize) {
+  char contentBuff[250];
+  
+  myFile = SD.open(fileName);
+  *fileSize = myFile.size();
+  
+  if (myFile) {
+    // Read from the file until there's nothing else in it
+    while (myFile.available()) {
+      strcat(contentBuff, myFile.read());
+    }
+    myFile.close(); // Close the file; only 1 can be open at a time
+  }
+  else {
+    Serial.println("Error opening file!");
+  }
+
+  return contentBuff;
 }
