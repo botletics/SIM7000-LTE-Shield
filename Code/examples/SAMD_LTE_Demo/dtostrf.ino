5e116c6508f9f940ba53b0f10f32aef510e3043d
Timothy Woo <botletics@gmail.com>
2018-11-11 22:39:55 -0500

    Create dtostrf.ino

---
+++ Code/examples/SAMD_LTE_Demo/dtostrf.ino
@@ -0,0 +1,76 @@
+/*
+  dtostrf - Emulation for dtostrf function from avr-libc
+  Copyright (c) 2015 Arduino LLC.  All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <stdio.h>
+
+#if 0
+char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
+  char fmt[20];
+  sprintf(fmt, "%%%d.%df", width, prec);
+  sprintf(sout, fmt, val);
+  return sout;
+}
+#else
+#include <string.h>
+#include <stdlib.h>
+char *dtostrf(double val, int width, unsigned int prec, char *sout)
+{
+  int decpt, sign, reqd, pad;
+  const char *s, *e;
+  char *p;
+  s = fcvt(val, prec, &decpt, &sign);
+  if (prec == 0 && decpt == 0) {
+  s = (*s < '5') ? "0" : "1";
+    reqd = 1;
+  } else {
+    reqd = strlen(s);
+    if (reqd > decpt) reqd++;
+    if (decpt == 0) reqd++;
+  }
+  if (sign) reqd++;
+  p = sout;
+  e = p + reqd;
+  pad = width - reqd;
+  if (pad > 0) {
+    e += pad;
+    while (pad-- > 0) *p++ = ' ';
+  }
+  if (sign) *p++ = '-';
+  if (decpt <= 0 && prec > 0) {
+    *p++ = '0';
+    *p++ = '.';
+    e++;
+    while ( decpt < 0 ) {
+      decpt++;
+      *p++ = '0';
+    }
+  }    
+  while (p < e) {
+    *p++ = *s++;
+    if (p == e) break;
+    if (--decpt == 0) *p++ = '.';
+  }
+  if (width < 0) {
+    pad = (reqd + width) * -1;
+    while (pad-- > 0) *p++ = ' ';
+  }
+  *p = 0;
+  return sout;
+}
+#endif
