a9151a2b2647e1e9722d1a409ca4d42cec51b9e1
Timothy Woo <botletics@gmail.com>
2018-11-11 22:03:02 -0500

    Added SAMD serial definition

--- Code/examples/FTP_Demo/FTP_Demo.ino
+++ Code/examples/FTP_Demo/FTP_Demo.ino
@@ -10,7 +10,7 @@
  *  
  *  Author: Timothy Woo (www.botletics.com)
  *  Github: https://github.com/botletics/SIM7000-LTE-Shield
- *  Last Updated: 11/5/2018
+ *  Last Updated: 11/11/2018
  *  License: GNU GPL v3.0
  */
 
@@ -21,6 +21,11 @@
 File myFile;
 
 const int CS_pin = 53;
+
+#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
+  // Required for Serial on Zero based boards
+  #define Serial SERIAL_PORT_USBVIRTUAL
+#endif
 
 // Define *one* of the following lines:
 //#define SIMCOM_2G // SIM800/808/900/908, etc.
